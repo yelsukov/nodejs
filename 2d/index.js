a175b27ad06f25ca6ee9c298481bbcdcbe1622fc
Yelsukov.Nikolay <yelsukov@gmail.com>
2016-11-27 02:31:53 +0700

    issue 2d - code optimization

--- 2d/index.js
+++ 2d/index.js
@@ -13,22 +13,17 @@ app.get('/2d', (req, res) => {
     try {
         if(/rgb/.test(color)) {
             let rgb = color.match(/^rgb\(\s*(\d{1,3})[,\s]+(\d{1,3})[,\s]+(\d{1,3})\s*\)/);
-            if(rgb) {
-                rgb.slice(1).map(v => {
-                    if(v < 256) { return parseInt(v) } else { throw new Error(""); }
-                });
-            } else {
+            if(rgb == null || rgb[1] > 255 || rgb[2] > 255 || rgb[3] > 255) {
                 throw new Error("");
             }
         } else if(/hsl/.test(color)) {
-            let rgb = color.match(/^hsl\(\s*(\d{1,3})[,\s]+(\d{1,3})\%[,\s]+(\d{1,3})\%\s*\)/);
-            if(rgb == null || rgb[1] > 255 || rgb[2] > 100 || rgb[3] > 100) {
+            let hsl = color.match(/^hsl\(\s*(\d{1,3})[,\s]+(\d{1,3})\%[,\s]+(\d{1,3})\%\s*\)/);
+            if(hsl == null || hsl[1] > 255 || hsl[2] > 100 || hsl[3] > 100) {
                 throw new Error("");
             }
         }
 
-        color = oneColor(color).hex();
-        res.send(color.toLowerCase());
+        res.send(oneColor(color).hex());
     } catch (e) {
         return res.send('Invalid color');
     }
